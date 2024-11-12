c6cc7cd997a73472e440961c381cac3a5e8b8808
So_o <sh8908may@gmail.com>
2024-11-13 02:35:22 +0300

    Add files via upload

---
+++ Variab..py
@@ -0,0 +1,5 @@
+homework_done = 12
+hours_spent= 1.5
+platform = 'Python'
+average_execution_time = hours_spent/homework_done
+print ('Курс:',platform,'; всего задач:',homework_done,'; затрачено часов:',hours_spent,'; среднее время выполнения:',average_execution_time)
